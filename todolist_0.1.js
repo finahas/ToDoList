eb79a98d269bf7d0acdb3f45e9b5ad7cde272bcd
finahas <54680842+finahas@users.noreply.github.com>
2019-11-03 17:56:35 +0530

    version 0.2

--- todolist_0.1.js
+++ todolist_0.1.js
@@ -1,12 +1,18 @@
-function fun1(){
 
-    var item = document.getElementById("input1").value;
-    var text = document.createTextNode(item);
-    var listelement = document.createElement("li");
-    listelement.appendChild(text);
-    document.getElementById("todolist").appendChild(listelement);
-
-}
-
-
-
+    $('input').keypress(function(key){
+        if(key.keyCode === 13){
+            var task = $('input').val();
+            //$('input').val("");
+            $('ul').append('<li>' + task + '<span><i class="trash icon"</i></span>');
+        }
+    });
+    
+    $('ul').on('click','span', function(){
+        $(this).parent().fadeOut(400,function(){
+            $(this).remove();
+        });
+    })    
+        
+    $('ul').on('click','li',function(){
+        $(this).toggleClass('done');
+    });
